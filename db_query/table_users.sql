e655f2488cf429771fee21a31c7bbdf85b9ae0c3
FrFernandez <frfernandez1996@gmail.com>
2019-05-13 16:20:59 -0400

    Add structure table users

---
+++ db_query/table_users.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL,
+  mail VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  status TINYINT(255) NOT NULL UNSIGNED,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  PRIMARY KEY id
+  );
