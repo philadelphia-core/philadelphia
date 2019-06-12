f058a84a9348f160ab245f028767f34b601c561f
@FrFernandez <fernandofernandezcontreras@gmail.com>
2019-06-12 09:44:29 -0400

    Update config default

--- config/config.php
+++ config/config.php
@@ -1,18 +1,19 @@
 <?php
 
   define('DB', array(
-    'driver' => '',
-    'host' => '',
-    'dbname' => '',
-    'username' => '',
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'srn',
+    'username' => 'root',
     'password' => '',
-    'chatset' => '',
+    'chatset' => 'utf8',
 
     'persistent' => true
   ));
 
   define('JWT', array(
-    'iss' => '',
-    'privateKey' => '',
-    'algorithm' => '',
+    'host' => 'http://localhost',
+    'privateKey' => '1asfrwqwkaoi21e9j12e1lasdlj12-asd',
+    'algorithm' => 'HS512',
   ));
